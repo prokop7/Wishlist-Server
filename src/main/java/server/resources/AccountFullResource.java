eb41a83344fa7a975ce376c4142b76d702258a41
prokop7 <a.prokopev@innopolis.ru>
2017-12-07 05:02:37 +0300

    Remove friends from "AccountFullResource"

--- src/main/java/server/resources/AccountFullResource.java
+++ src/main/java/server/resources/AccountFullResource.java
@@ -7,21 +7,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class AccountFullResource extends AccountCommonResource {
-    private List<AccountCommonResource> friends = new LinkedList<>();
+    //TODO Really not needed?
+//    private List<AccountCommonResource> friends = new LinkedList<>();
     private List<WishlistResource> wishlists;
 
     public AccountFullResource(Account account) {
         super(account);
-        account.getFriends().forEach(friend -> friends.add(new AccountCommonResource(friend)));
+//        account.getFriends().forEach(friend -> friends.add(new AccountCommonResource(friend)));
         List<Wishlist> list = account.getWishlists();
         List<WishlistResource> wishlists = new LinkedList<>();
         list.forEach(wishlist -> wishlists.add(new WishlistResource(wishlist)));
         this.wishlists = wishlists;
     }
 
-    public List<AccountCommonResource> getFriends() {
-        return friends;
-    }
+//    public List<AccountCommonResource> getFriends() {
+//        return friends;
+//    }
 
     public List<WishlistResource> getWishlists() {
         return wishlists;
