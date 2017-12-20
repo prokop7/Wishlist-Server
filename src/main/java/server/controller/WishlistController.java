b720c50b20f027ba122346b565292e5a7d75b478
Anton Prokopev <a.prokopev@innopolis.ru>
2017-12-20 21:21:31 +0300

    Added editing wishlists. Fixed wishlist duplication.

--- src/main/java/server/controller/WishlistController.java
+++ src/main/java/server/controller/WishlistController.java
@@ -11,10 +11,7 @@ import server.model.Account;
 import server.model.Wishlist;
 import server.persistence.AccountRepository;
 import server.persistence.WishlistRepository;
-import server.resources.AccountCommonResource;
-import server.resources.ItemResource;
-import server.resources.Mapper;
-import server.resources.WishlistResource;
+import server.resources.*;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -46,14 +43,24 @@ public class WishlistController {
     List<WishlistResource> getWishlists(@PathVariable int userId,
                                         @RequestAttribute Claims claims) {
         validateUserId(userId);
-        List<Wishlist> wishlists;
+        List<Wishlist> wishlists = new ArrayList<>();
         int roleId = Integer.valueOf(claims.getSubject());
-        wishlists = roleId != userId
-                ? wishlistRepository.getAllWithVisibility(userId, roleId)
-                : wishlistRepository.getAllByAccount_Id(userId);
+        for (Wishlist w : wishlistRepository.getAllByAccount_Id(userId)) {
+            boolean inExclusion = false;
+            for (Account account : w.getExclusions()) {
+                if (account.getId() == roleId) {
+                    inExclusion = true;
+                    break;
+                }
+            }
+            if ((w.getVisibility().getValue() == 0) == inExclusion || roleId == userId)
+                wishlists.add(w);
+        }
         List<WishlistResource> resources = new LinkedList<>();
         for (Wishlist wishlist : wishlists) {
-            WishlistResource wishlistResource = new WishlistResource(wishlist);
+            WishlistResource wishlistResource = roleId == userId
+                    ? new PrivateWishlistResource(wishlist)
+                    : new WishlistResource(wishlist);
             for (ItemResource itemResource : wishlistResource.getItems()) {
                 if (itemResource.getTaker() != null) {
                     if (itemResource.getTaker().getId() == roleId)
@@ -75,7 +82,7 @@ public class WishlistController {
         List<Account> exclusions = new ArrayList<>();
         for (AccountCommonResource exclusion : wishlistResource.getExclusions()) {
             exclusions.add(accountRepository.findAccountById(exclusion.getId()).orElseThrow(
-                            () -> new UserNotFoundException(exclusion.getId())));
+                    () -> new UserNotFoundException(exclusion.getId())));
         }
         Wishlist wishlist = mapper.map(wishlistResource);
         wishlist.setAccount(accountRepository.getOne(userId));
@@ -90,6 +97,27 @@ public class WishlistController {
                     return ResponseEntity.created(loc).build();
                 }).orElse(ResponseEntity.noContent().build());
     }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/{wishlistId}")
+    ResponseEntity<?> editWishlist(@PathVariable int userId,
+                                   @PathVariable int wishlistId,
+                                   @Valid @RequestBody WishlistResource wishlistResource) {
+        validateUserId(userId);
+        validateWishlistId(wishlistId);
+        List<Account> exclusions = new ArrayList<>();
+        for (AccountCommonResource exclusion : wishlistResource.getExclusions()) {
+            exclusions.add(accountRepository.findAccountById(exclusion.getId()).orElseThrow(
+                    () -> new UserNotFoundException(exclusion.getId())));
+        }
+        Wishlist wishlist = wishlistRepository.findByAccount_IdAndId(userId, wishlistId).orElseThrow(
+                () -> new WishlistNotFoundException(wishlistId));
+        mapper.map(wishlistResource, wishlist);
+        wishlist.setExclusions(exclusions);
+        Wishlist res = wishlistRepository.save(wishlist);
+        return wishlistRepository.findById(res.getId()).map(
+                account -> ResponseEntity.ok(res)).orElse(ResponseEntity.noContent().build());
+    }
+
 
     @RequestMapping(method = RequestMethod.GET, value = "/{wishlistId}")
     WishlistResource getWishlist(@PathVariable int userId,
